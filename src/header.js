a05f6b145a262260dcd8c72e7d3ee9c48c92abd3
Fahad Ghaffar <fahad.ghaffar@lslogics.local>
2024-02-20 14:26:13 +0500

    header

--- src/header.js
+++ src/header.js
@@ -7,6 +7,9 @@ return (
 
     <header>
     <h1 className="mt-4 p5 bg-primary text-white rounded">Header</h1>
+    <h1>Team Member Allocation</h1>
+    <h3>{selectedTeam}  has {teamMemberCount} Member</h3>
+
     </header>
 );
 
